368763b08d69430ba3d0283d7f889bdeea5b9cee
Fran <franciscooberto@gmail.com>
2021-04-14 17:25:23 -0300

    Agrego lock

--- Libro.java
+++ Libro.java
@@ -1,14 +1,17 @@
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Libro {
+    private ReentrantReadWriteLock lock;
     private static Integer nBook = 0;
-    public Boolean isFinalVersion;
-    public Integer reviews;
-    public Integer reads;
+    private Boolean isFinalVersion;
+    private Integer reviews;
+    private Integer reads;
     private Integer id;
 
     public Libro(){
+        this.lock = new ReentrantReadWriteLock();
         this.isFinalVersion = false;
         this.reviews = 0;
         this.reads = 0;
@@ -20,29 +23,35 @@ public class Libro {
     }
 
     public void write() {
+        lock.writeLock().lock();
         try{
             TimeUnit.MILLISECONDS.sleep(new Random().nextInt(1));
             this.reviews++;
             if (reviews >= 10){ isFinalVersion = true; }
         }catch (InterruptedException e){
             e.printStackTrace();
+        }finally {
+            lock.writeLock().unlock();
         }
     }
 
     public Boolean read() {
+        lock.readLock().lock();
         try{
             TimeUnit.MILLISECONDS.sleep(new Random().nextInt(1));
             this.reads++;
         }catch (InterruptedException e){
             e.printStackTrace();
         }finally {
+            lock.readLock().unlock();
             if (isFinalVersion){return true;}
             return false;
         }
     }
 
     public String toString(){
-        return "Libro " + this.id;
+        String string "Libro ID(" + this.id + ")("
+        return ;
     }
 
 }
